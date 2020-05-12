0052ce9704bf01772fc3e0de8b07e9ccb7bae84c
Yash Jaiswal <jais.yash01@gmail.com>
2020-05-12 11:56:33 +0530

    login page front end

--- client/src/users/login.js
+++ client/src/users/login.js
@@ -1,17 +1,70 @@
 import React from 'react'
+import axios from '../config/axios'
+import Swal from 'sweetalert2'
 
 class Login extends React.Component {
     constructor() {
         super()
         this.state = {
             email: '',
-            password: ''
+            password: '',
         }
+    }
+    handleChange = (e) => {
+        this.setState({ [e.target.id]: e.target.value })
+    }
+    handleSubmit = (e) => {
+        e.preventDefault()
+        const formData = {}
+        formData.email = this.state.email
+        formData.password = this.state.password
+        axios.post('/users/login', formData)
+            .then(response => {
+                if (response.data.hasOwnProperty('errors')) {
+                    // console.log(response.data.message)
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Please enter valid values',
+                        text: 'Validation failed',
+                    })
+                } else {
+                    // console.log(response.data, 'in else')
+                    Swal.fire(
+                        'Good job!',
+                        'Successfully created account',
+                        'success'
+                    )
+                    // console.log(response.data)
+                    const token = response.data.token
+                    localStorage.setItem('authUrlToken', token)
+                    this.props.history.push('/')
+                }
+            })
+            .catch(err => {
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Error',
+                    text: err,
+                })
+            })
     }
     render() {
         return (
             <div>
-                <h1> Loginnn </h1>
+                <br />
+                <h2 className="text-xl-center">Login </h2>
+                <br />
+                <form onSubmit={this.handleSubmit}>
+                    <div className="form-group">
+                        <label htmlFor="email">Email address</label>
+                        <input className="form-control" type="input" id="email" name="email" value={this.state.email} onChange={this.handleChange} />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="password">Password</label>
+                        <input className="form-control" type="password" id="password" name="password" value={this.state.password} onChange={this.handleChange} />
+                    </div>
+                    <button type="submit" className="btn btn-primary">Submit</button>
+                </form>
             </div>
         )
     }
