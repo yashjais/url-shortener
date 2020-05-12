d2074ce29dcc38f2150dd1971c219db04922d4e4
Yash Jaiswal <jais.yash01@gmail.com>
2020-05-13 00:27:44 +0530

    refactored

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
 const setUpDB = require('./config/database')
-const port = 3010
+// const port = 3010
+const port = process.env.PORT || 3000
+const path = require('path')
 const router = require('./config/routes')
 const cors = require('cors')
 
@@ -10,6 +12,11 @@ app.use(express.json())
 app.use('/', router)
 
 setUpDB()
+
+app.use(express.static(path.join(__dirname, "client/build")))
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname + "/client/build/index.html"))
+})
 
 app.listen(port, () => {
     console.log('listening on the port', port)
